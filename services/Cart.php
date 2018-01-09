2941f267006837d05dd093c5c33f1ade4b66eb85
Terry <2358269014@qq.com>
2018-01-09 22:45:53 +0800

    init file frame

---
+++ services/Cart.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * FecShop file.
+ *
+ * @link http://www.fecshop.com/
+ * @copyright Copyright (c) 2016 FecShop Software LLC
+ * @license http://www.fecshop.com/license/
+ */
+
+namespace fecshop\rediscart\services;
+
+use Yii;
+
+/**
+ * Cart services. 购物车service， 执行购物车部分对应的方法。
+ * @author Terry Zhao <2358269014@qq.com>
+ * @since 1.0
+ */
+class Cart extends \fecshop\services\Cart
+{
+    
+}
---
+++ services/cart/Quote.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * FecShop file.
+ *
+ * @link http://www.fecshop.com/
+ * @copyright Copyright (c) 2016 FecShop Software LLC
+ * @license http://www.fecshop.com/license/
+ */
+
+namespace fecshop\rediscart\services\cart;
+
+//use fecshop\models\mysqldb\Cart as MyCart;
+use fecshop\services\Service;
+use Yii;
+
+/**
+ * Cart services. 对购物车操作的具体实现部分。
+ * @author Terry Zhao <2358269014@qq.com>
+ * @since 1.0
+ */
+class Quote extends \fecshop\services\cart\Quote
+{
+    
+}
